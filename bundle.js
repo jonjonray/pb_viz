5e228c6698bdffa6535dfe785d722725e98e46b1
jonjonray <jonjonray@gmail.com>
2017-12-08 09:16:16 -0800

    final commit

--- bundle.js
+++ bundle.js
@@ -90,23 +90,16 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 document.addEventListener("DOMContentLoaded", function () {
-    // const canvasEl = document.getElementsByTagName("canvas")[0];
-    // canvasEl.width = "1324";
-    // canvasEl.height = "1500";
-    // const ctx = canvasEl.getContext("2d");
-    // ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-    // // ctx.fillStyle = "#f4b042";
-    // // ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
-    //
-    // const board = new Board(Financials,ctx,canvasEl);
 
-    var simulation = d3.forceSimulation().force("forceX", d3.forceX().strength(.05).x("650")).force("forceY", d3.forceY().strength(.05).y("650")).force("center", d3.forceCenter().x("650").y("650")).force("charge", d3.forceManyBody().strength(-40)).force("collide", d3.forceCollide().radius(function (d) {
+    var simulation = d3.forceSimulation().force("forceX", d3.forceX().strength(0.04).x("650")).force("forceY", d3.forceY().strength(0.03).y("1000"))
+    // .force("center", d3.forceCenter().x("650").y("1000"))
+    .force("charge", d3.forceManyBody().strength(-50)).force("collide", d3.forceCollide().radius(function (d) {
         return d.r;
     }).iterations(0));
 
-    var svg = d3.select("#root").append("svg").attr("width", "1300px").attr("height", "1300px");
+    var svg = d3.select("#root").append("svg").attr("width", "1300px").attr("height", "2000px");
 
-    var rscale = d3.scaleLinear().domain([1, 731]).range([10, 70]);
+    var rscale = d3.scaleLinear().domain([1, 731]).range([5, 80]);
 
     var node = svg.selectAll('.circles').data(_financials2.default).enter().append('circle').attr('r', function (d) {
         return rscale(d["Market Cap"]);
--- src/entry.jsx
+++ src/entry.jsx
@@ -4,21 +4,13 @@ import * as d3 from "d3";
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  // const canvasEl = document.getElementsByTagName("canvas")[0];
-  // canvasEl.width = "1324";
-  // canvasEl.height = "1500";
-  // const ctx = canvasEl.getContext("2d");
-  // ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-  // // ctx.fillStyle = "#f4b042";
-  // // ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
-  //
-  // const board = new Board(Financials,ctx,canvasEl);
+
 
   var simulation = d3.forceSimulation()
-      .force("forceX", d3.forceX().strength(.05).x("650"))
-      .force("forceY", d3.forceY().strength(.05).y("650"))
-      .force("center", d3.forceCenter().x("650").y("650"))
-      .force("charge", d3.forceManyBody().strength(-40))
+      .force("forceX", d3.forceX().strength(0.04).x("650"))
+      .force("forceY", d3.forceY().strength(0.03).y("1000"))
+      // .force("center", d3.forceCenter().x("650").y("1000"))
+      .force("charge", d3.forceManyBody().strength(-50))
       .force("collide", d3.forceCollide().radius(function(d) { return d.r; }).iterations(0));
 
 
@@ -26,13 +18,13 @@ document.addEventListener("DOMContentLoaded", () => {
   var svg = d3.select("#root")
 	.append("svg")
 	.attr("width", "1300px")
-	.attr("height","1300px");
+	.attr("height","2000px");
 
 
 
   var rscale = d3.scaleLinear()
             .domain([1,731])
-            .range([10,70]);
+            .range([5,80]);
 
 
 
